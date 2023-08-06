7bd0c4c673b215718a87c02e9242a044710a2687
codenameMANUEL <manuelfranky0@gmail.com>
2023-08-06 14:53:35 +0100

    ATM MODEL

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const password = 1010;
 const inputField = prompt("Enter Your Pin \n" + "DON'T SHARE YOUR PIN WITH ANYONE !!!");
 
 if(inputField == password){
-   let balancechecker = prompt("Enter the amount you want to withdraw");
+   let balancechecker = prompt("Enter the amount you want to withdraw")
     if(balancechecker <= currentBalance){
         alert("RECEIVING....");
         alert("You Just Withdraw " + balancechecker + "\n" + "\n" + "New Balance is " + (currentBalance - balancechecker));
